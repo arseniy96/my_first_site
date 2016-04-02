d8feedac45fcc14aac36f3d9e295963418724b58
arseniy96 <arsenzhar@gmail.com>
2016-04-02 23:13:18 +0300

    Add comments#create to comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    if params[:article_id] != nil
+      @article = Article.find(params[:article_id])
+      @article.comments.new(comments_params)
+      @article.save
+      redirect_to article_path(@article)
+    elsif params[:question_id] != nil
+      @question = Question.find(params[:question_id])
+      @question.comments.new(comments_params)
+      @question.save
+      redirect_to question_path(@question)
+    end
+  end
+
+  private
+
+  def comments_params
+    params.require(:comment).permit(:text)
+  end
 end
