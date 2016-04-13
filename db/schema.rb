b33148989b4ecb0db1a242d134b75c00eae08abc
arseniy96 <arsenzhar@gmail.com>
2016-04-13 20:17:25 +0300

    Add column image to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407202836) do
+ActiveRecord::Schema.define(version: 20160413171335) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20160407202836) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "image"
+    t.integer  "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "people", ["users_id"], name: "index_people_on_users_id"
 
   create_table "questions", force: :cascade do |t|
     t.text     "question"
@@ -51,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160407202836) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
