71de25944354c464b4416213503c9479b1ae5cbd
arseniy96 <arsenzhar@gmail.com>
2016-04-15 22:31:28 +0300

    Display user articles and questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = Article.includes(:user)
+    @questions = Question.includes(:user)
+  end
 end
