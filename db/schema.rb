a2fe29b197f7e7ae775d201bc20d27ce4812e3e2
arseniy96 <arsenzhar@gmail.com>
2016-04-18 20:16:40 +0300

    Add column avatar to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413171335) do
+ActiveRecord::Schema.define(version: 20160418164722) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -54,6 +54,10 @@ ActiveRecord::Schema.define(version: 20160413171335) do
     t.datetime "updated_at",                          null: false
     t.string   "username"
     t.string   "image"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
