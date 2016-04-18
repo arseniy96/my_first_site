1d48912e29e84ac38400e67465d92b2c12c7a03d
arseniy96 <arsenzhar@gmail.com>
2016-04-19 00:07:15 +0300

    Add validations to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
   has_many :comments, as: :commentable
   belongs_to :user
+
+  validates :title, presence: true, length: {maximum: 140}
+  validates :text, presence: true, length: {maximum: 4000}
+
 end
