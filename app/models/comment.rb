a50ee594ae50bc3bd95bf2450fc4abb104c7365c
arseniy96 <arsenzhar@gmail.com>
2016-05-16 20:54:44 +0300

    Add validations for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
+  validates :text, presence: true, length: {maximum: 2500}
+  validates :username, presence: true
 end
