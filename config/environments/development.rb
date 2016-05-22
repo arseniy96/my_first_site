9b448ee7d4ef273c901276a0d64e982e7be4615c
arseniy96 <arsenzhar@gmail.com>
2016-05-23 00:05:25 +0300

    Add confirmation email for registration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ Rails.application.configure do
   Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "127.0.0.1", :port => 1025}
 
   # Settings specified here will take precedence over those in config/application.rb.
 
