6c320314a148261383873989888b4f3cfb2145ab
arseniy96 <arsenzhar@gmail.com>
2016-05-31 11:46:28 +0300

    Add a choice categories to articles views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,6 +56,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :category_id)
   end
 end
